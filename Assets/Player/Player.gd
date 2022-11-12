013dae65117f44b2fd484d36db197d6d847f2e78
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-11-12 20:21:39 +0800

    Added being able to roll

--- Assets/Player/Player.gd
+++ Assets/Player/Player.gd
@@ -8,6 +8,7 @@ extends KinematicBody2D
 const MAX_SPEED = 80
 const ACCELERATION = 500
 const FRICTION = 500
+const ROLL_SPEED = 120
 
 enum {
 	MOVE,
@@ -16,7 +17,7 @@ enum {
 
 var state = MOVE
 var velocity = Vector2.ZERO
-
+var roll_vector = Vector2.LEFT
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
@@ -32,7 +33,7 @@ func _process(delta: float) -> void:
 		ATTACK:
 			attack_state(delta)
 		ROLL:
-			pass
+			roll_state(delta)
 
 func move_state(delta:float)->void:
 	var input_vector = Vector2.ZERO
@@ -40,21 +41,32 @@ func move_state(delta:float)->void:
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position",input_vector)
 		animationTree.set("parameters/Run/blend_position",input_vector)
 		animationTree.set("parameters/Attack/blend_position",input_vector)
+		animationTree.set("parameters/Roll/blend_position",input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector*MAX_SPEED,ACCELERATION*delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO,FRICTION*delta)
-	velocity = move_and_slide(velocity)
-	
+	Move()
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
-	
+func roll_state(delta:float):
+	velocity = roll_vector*ROLL_SPEED
+	animationState.travel("Roll")
+	Move()
 func attack_state(delta:float):
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
+func Move():
+	velocity = move_and_slide(velocity)
+func roll_animation_finished():
+	velocity = velocity * 0.8
+	state = MOVE
 func attack_animation_finished():
 	state = MOVE
